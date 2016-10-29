7ebaf5d1503f440162a89394424617939b01f104
Marc Cenac <mcenac@boundlessgeo.com>
2016-10-29 11:10:54 -0500

    adding test to check that our evacuspot data is present in the city's gis dataset

--- evacuspots_test.py
+++ evacuspots_test.py
@@ -1,5 +1,6 @@
 import json
 import unittest
+import urllib2
 
 class TestEvacuspotJson(unittest.TestCase):
     required_fields = [
@@ -35,6 +36,13 @@ class TestEvacuspotJson(unittest.TestCase):
             required_props = [p for p in self.required_fields if p is not 'longitude' and p is not 'latitude']
             for prop in required_props:
                 self.assertTrue(prop in feature['properties'], '{} is required but missing from properties'.format(prop))
+
+    def test_evacuspots_exist_on_city_gis(self):
+        response = urllib2.urlopen('http://gis.nola.gov:6080/arcgis/rest/services/PublicSafety/Evacuspots/MapServer/0/query?where=1%3D1&outFields=*&f=geojson')
+        city_evacuspots = json.loads(response.read())
+        city_evacuspot_names = [es['properties']['FACNAME'] for es in city_evacuspots['features']]
+        for evacuspot in self.evacuspots:
+            self.assertTrue(evacuspot['name'] in city_evacuspot_names, '{} is not in the city\'s gis dataset'.format(evacuspot['name']))
 
 if __name__ == '__main__':
     unittest.main()
